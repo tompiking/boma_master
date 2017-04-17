20b23f6e18d547746d52b6427743cea77dae9c51
zjy <664844970@qq.com>
2017-04-17 17:06:58 +0800

    newchange

--- src/main/java/com/qingke/boma/util/Const.java
+++ src/main/java/com/qingke/boma/util/Const.java
@@ -3,7 +3,7 @@ package com.qingke.boma.util;
 public class Const {
 	public static final String APP_PATH = "APP_PATH";
 	public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
-	public static final String PATH_PIC = "C:\\Users\\Administrator\\Pictures\\";
+	public static final String PATH_PIC = "C:\\Users\\qingke05\\picture\\";
 	// 分页每页显示的条数
 	public static final Integer pageAmount = 4;
 	public static final Integer pageAmount_case = 2;
